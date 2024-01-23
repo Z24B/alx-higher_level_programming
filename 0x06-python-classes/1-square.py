c7618bd8767986c5f09991ded96c3cdf67d86a09
Z24B <zivha.beharie@gmail.com>
2024-01-23 03:11:09 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,15 +2,14 @@
 """Class Square defines square"""
 
 class Square:
-    """
-    This class defines a square.
-
-    Attributes:
-    - __size (int): Private instance attribute representing the size of the square.
-    """
+    """This class defines a square."""
     def __init__(self, size):
         """
         Initializes a new instance of the Square class.
+
+        Attributes:
+        - __size (int): Private instance attribute representing
+        the size of the square.
 
         Parameters:
         - size (int): The size of the square.
