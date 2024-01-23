b43499ca7b8aa254a5dbe28ab2690f98a381501b
Z24B <zivha.beharie@gmail.com>
2024-01-23 02:54:57 -0800

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+"""Define a class Square"""
+
 
 class Square:
-    """This class defines a square."""
+    """Class that defines properties of square.
+    """
     pass
