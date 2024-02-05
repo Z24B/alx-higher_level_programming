ad3690b87246922795e145726f74a362d7d42eb2
Z24B <zivha.beharie@gmail.com>
2024-02-05 08:08:29 -0800

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""function that returns true or false"""
+"""Function that returns true or false"""
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is an inherited class"""
-    return (issubclass(type(obj), a_class))
+    """Return True if obj is instance of class inherited, or False"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
+
+
+if __name__ == "__main__":
+    # Test the function
+    class ParentClass:
+        pass
+
+    class ChildClass(ParentClass):
+        pass
+
+    example_object = ChildClass()
+    result = inherits_from(example_object, ParentClass)
+    print(result)
