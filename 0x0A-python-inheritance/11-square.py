faf3b4cf1577de54cdb9859154a3c8561467350e
Z24B <zivha.beharie@gmail.com>
2024-02-05 09:06:33 -0800

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize the Square with size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description."""
+        return f"[Square] {self._Rectangle__width}"
