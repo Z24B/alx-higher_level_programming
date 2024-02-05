9b5644245cde8ccd36bd15dec005ed05440a039f
Z24B <zivha.beharie@gmail.com>
2024-02-05 07:52:57 -0800

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""function that returns True or False"""
+"""function that returns true or false"""
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is an instance of a class, otherwise false"""
-    if issubclass(obj.__class__, a_class) is True:
-        if obj.__class__ is not a_class:
-            return True
-    else:
-        return False
+    """returns true if obj is an inherited class"""
+    return (issubclass(type(obj), a_class))
