3500b8048b422415023fa927a5e0c78d47d17af6
Z24B <zivha.beharie@gmail.com>
2024-02-06 04:36:42 -0800

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""File with class Student that defines a student"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes student instance with first name, last name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+        return new_dict
