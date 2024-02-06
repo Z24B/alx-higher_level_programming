6fb7311183560b57d7b7fb4a1dbfd62a47c61477
Z24B <zivha.beharie@gmail.com>
2024-02-06 02:30:08 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""File containing function for read_file"""
 
 
 def read_file(filename=""):
     """Reads content of a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
