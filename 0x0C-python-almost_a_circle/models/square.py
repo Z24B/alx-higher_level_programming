80c5cb8cdf1406400cb068ff2e4d19cc3d8a3a08
Zivha <zivha.beharie@gmail.com>
2024-02-11 02:36:04 -0800

    Task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,6 +46,10 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+
     def __str__(self):
         """Return a string representation of the Square instance."""
         return "[Square] ({}) {}/{} - {}".format(
