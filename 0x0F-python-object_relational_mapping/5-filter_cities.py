4ecbba00eb931f86d885787b129ee2ef333843c4
Z24B <zivha.beharie@gmail.com>
2024-04-13 00:03:14 -0700

    Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script that lists all cities of a given state"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    state_name = argv[4]
+
+    #Connect to MySQL server
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8"
+            )
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute SQL query
+    query = """
+    SELECT GROUP_CONCAT(DISTINCT cities.name SEPARATOR ', ')
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cur.execute(query, (state_name,))
+
+    # Fetch the result
+    result = cur.fetchone()[0]
+    if result:
+        print(result)
+    else:
+        print("")
+    cur.close()
+    db.close()
