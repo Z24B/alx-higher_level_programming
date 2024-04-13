9586725faf7db97772a9195ab38153e7e1f38b5e
Z24B <zivha.beharie@gmail.com>
2024-04-13 00:35:21 -0700

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,16 +4,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
+    # MySQL connection parameters
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
     state_name = argv[4]
 
     # Connect to MySQL server
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
+            user=username,
+            passwd=password,
+            db=database,
             charset="utf8"
             )
 
@@ -32,6 +35,8 @@ if __name__ == "__main__":
 
     # Fetch the result
     result = cur.fetchone()[0]
+
+    # Display results
     if result:
         print(result)
     else:
