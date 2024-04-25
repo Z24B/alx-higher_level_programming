99a0780c9d0b8d058873f9d90de63b91454922be
Z24B <zivha.beharie@gmail.com>
2024-04-25 04:38:03 -0700

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes URL, sends GET request and displays the body of a 200 status code response
-curl -s -X GET -L "$1" -o /dev/null -w "%{http_code}" | grep -q 200 && curl -s "$1"
+curl -Ls "$1"
