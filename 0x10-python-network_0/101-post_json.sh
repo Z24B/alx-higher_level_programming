2710d61092c7f33d0f944b34283ec06728d27da1
Z24B <zivha.beharie@gmail.com>
2024-04-25 05:19:54 -0700

    Task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# script sends a JSON POST request to a URL with the contents of a file as the body
-if [ -f "$2" ]; then
-	curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-	echo ""
-else
-	echo "File not found or invalid."
-fi
+# script sends a JSON POST request to a URL with the contents of a file as body
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
