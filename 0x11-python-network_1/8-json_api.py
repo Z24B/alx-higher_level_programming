569d555d50cf24b32306b015592159e70b3b8dbc
Z24B <zivha.beharie@gmail.com>
2024-04-26 02:29:40 -0700

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Sends a POST request to a URL with a letter as a parameter and
+displays the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+    response = requests.post(url, data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
