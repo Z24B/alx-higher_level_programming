8003a5558eca8be545c3abe595b90380b25a41c7
Z24B <zivha.beharie@gmail.com>
2024-05-21 06:36:41 -0700

    Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const tasks = JSON.parse(body);
+    const completedTasksByUser = {};
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId]++;
+        } else {
+          completedTasksByUser[task.userId] = 1;
+        }
+      }
+    });
+    console.log(completedTasksByUser);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
