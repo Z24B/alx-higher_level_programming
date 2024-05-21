d8303fcc94e84f2486065e304037dde20cd754b1
Z24B <zivha.beharie@gmail.com>
2024-05-21 06:19:42 -0700

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
-const wedgeAntillesId = '18';
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
+    const wedgeAntillesId = '18';
     let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-        count++;
+
+    // Loop through each film and check if Wedge Antilles is present
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes(`/api/people/${wedgeAntillesId}/`)) {
+          count++;
+          break;
+        }
       }
-    });
+    }
     console.log(count);
   } else {
     console.log(`Error: ${response.statusCode}`);
