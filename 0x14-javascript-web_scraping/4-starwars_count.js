66b3de75ef411a6524ede1dcc0cf47ec41b09e5b
Z24B <zivha.beharie@gmail.com>
2024-05-21 06:17:21 -0700

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+const wedgeAntillesId = '18';
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
